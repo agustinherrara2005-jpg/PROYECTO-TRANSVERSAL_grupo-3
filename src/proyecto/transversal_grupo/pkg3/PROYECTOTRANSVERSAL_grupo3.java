010bf6c74f3d1752b5c34aeaafcb81637638db13
AgusH <agustin.herrara2005@gmail.com>
2025-10-02 19:44:09 -0300

    tp

---
+++ src/proyecto/transversal_grupo/pkg3/PROYECTOTRANSVERSAL_grupo3.java
@@ -0,0 +1,17 @@
+
+package proyecto.transversal_grupo.pkg3;
+
+/**
+ *
+ * @author Admin
+ */
+public class PROYECTOTRANSVERSAL_grupo3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    //hola
+}
