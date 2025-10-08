3efcfa1290291bd8525edaf1abfcef02a11186b8
Kamil Jofré <kamiljelias@gmail.com>
2025-10-08 17:42:09 -0300

    Update Main.java

--- src/Vista/Main.java
+++ src/Vista/Main.java
@@ -8,22 +8,22 @@ import java.time.LocalDate;
 
 
 public class Main {
-        public static void main(String[] args) {
-            LocalDate fecha = LocalDate.now();
-            Alumno estudioso = new Alumno("Cristy", fecha, false); // entidad
+    public static void main(String[] args) {
+        LocalDate fecha = LocalDate.now();
+        Alumno estudioso = new Alumno("Cristy", fecha, false); // entidad
 
-            new Main().conectar(estudioso);
+        new Main().conectar(estudioso);
 
-            System.out.println("Alumno " + estudioso.getNombre() + " guardado con éxito");
-        }
-
-        void conectar(Alumno estudioso) {
-            sbConexion conexion = new sbConexion("jdbc:mysql://localhost/universidadulp_3", "root", "");
-            AlumnoData alumnoData = new AlumnoData(conexion);
-
-            alumnoData.guardarAlumno(estudioso);
-            Alumno alu = alumnoData.buscarAlumno(estudioso.getIdAlumno());
-
-            System.out.println("Datos: " + alu);
-        }
+        System.out.println("Alumno " + estudioso.getNombre() + " guardado con éxito");
     }
+
+    void conectar(Alumno estudioso) {
+        sbConexion conexion = new sbConexion("jdbc:mysql://localhost/", "root", "");
+        AlumnoData alumnoData = new AlumnoData(conexion);
+
+        alumnoData.guardarAlumno(estudioso);
+        Alumno alu = alumnoData.buscarAlumno(estudioso.getIdAlumno());
+
+        System.out.println("Datos: " + alu);
+    }
+}
